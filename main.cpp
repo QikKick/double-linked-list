9f8ec2f160e224c649618acdcf68fc71390e679c
Emilis Keras <Emilis_Keras@epam.com>
2024-02-17 16:51:18 +0200

    Pirma užduotis + Iš failo

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 #include <string>
-int main(int argc, char *argv[]) {
-    std::string fileName = std::string(argv[1]) + ".txt";
-    std::cout << "Creating file: " << fileName << std::endl;
+#include <fstream>
 
-    return 0;
-}
+    int main(int argc, char *argv[]) {
+        if(argc > 1) {
+            std::string fileName = std::string(argv[1]) + ".txt";
+            std::cout << "Reading file: " << fileName << std::endl;
+
+            std::ifstream file(fileName);
+            if(file.is_open()) {
+                std::string line;
+                while(std::getline(file, line)) {
+                    std::cout << line << std::endl;
+                }
+                file.close();
+            } else {
+                std::cout << "Unable to open file." << std::endl;
+            }
+            std::ifstream fileIn(fileName);
+            if(file.is_open()) {
+                std::string line;
+                while(std::getline(file, line)) {
+                    std::cout << line << std::endl;
+                }
+                file.close();
+            } else {
+                std::cout << "Unable to open file." << std::endl;
+            }
+
+        } else {
+            std::cout << "No arguments provided." << std::endl;
+        }
+
+
+
+        return 0;
+    }
